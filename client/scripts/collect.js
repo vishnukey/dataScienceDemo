621af4242d21ff24520c4e8f2a319a4227b2e7d4
Riley <vishnukey@gmail.com>
2018-09-09 22:01:59 -0600

    adding documentation to all existing script files

--- client/scripts/collect.js
+++ client/scripts/collect.js
@@ -1,35 +1,49 @@
+/*
+ * when the colour input selector has chnaged, send its new value to the server
+ */
 function update(){
   // get value for input (id: #color)
-  const elem = document.querySelector("#colour")
-  const colour = elem.value
-  console.log(colour)
+  const elem = document.querySelector("#colour") //get the colour input selector
+  const colour = elem.value // get the inputs value
 
-  const res = postData("./update", {colour})
+  const res = postData("./update", {colour}) // send the colour to the server
 }
 
+/*
+ * POST some data to a url encoded as jsonData
+ *
+ * @param url the destination of the POST request
+ * @param data the data to be sent
+ * @param extraOptions additional options to be sent to the fetch function
+ * @param extraHeaders additional headers for the POST request
+ * @return a promise to the response data, as an object
+ */
 async function postData(url = ``, data = {}, extraOptions = {}, extraHeaders = {}) {
-  // Default options are marked with *
+  // Form the options for the request
+
+  // sending data encoded as json
   const baseHeaders = {
           "Content-Type" : "application/json"
   }
 
+  // add any additional headers that there might be
   const headers = Object.assign(baseHeaders, extraHeaders)
 
+  // encode the data as json
   const body = JSON.stringify(data)
 
+ // minial object to pass to fetch for a POST request
   const baseOptions = {
           method: "POST",
           headers,
           body
   }
 
+  // add any addition options that there might be
   const options = Object.assign(baseOptions, extraOptions)
 
-  console.log(options)
-
-  const response = await fetch(url, options)
-
-  const json = await response.json()
+  const response = await fetch(url, options) // send the request
+  const json = await response.json() //covnert response body to an object
 
   return json
 }
--- client/scripts/visual.js
+++ client/scripts/visual.js
@@ -1,8 +1,13 @@
 const POINT_SIZE = 5
 
+/*
+ * this function is called when the web page is done loading
+ * it sets up the canvas, performs an initial draw and then
+ * sets up a the canvas to be re-rendered every 3 seconds
+ */
 async function ready(){
-  const canvas = document.querySelector("#cnv")
-  const ctx = canvas.getContext("2d")
+  const canvas = document.querySelector("#cnv") // get the canvas
+  const ctx = canvas.getContext("2d") // get the rendering contex
 
   const renderFunc = makeRenderFunc(ctx)
 
@@ -11,35 +16,60 @@ async function ready(){
   const looper = setInterval(renderFunc, 3000)
 }
 
+/*
+ * given a context, produces a function that will render the points to the canvas
+ * @param ctx the context object of a canvas to be draw to
+ * @return an asynchornous function that draws to the canvas
+ */
 function makeRenderFunc(ctx){
+
+  /*
+   * an asynchornous function that fetches new data if it exists and then
+   * renders the data to the canvas if it exists
+   *
+   * @param force forces the drawing of data even if there is no new Data
+   */
   return async function(force = false){
     //fetch the points form /points
     const {newData, points} = await getPoints(force)
-    console.log(points)
 
+    // quit if there is no new data and rendering isn't forced
     if (!newData && !force) return
 
     //render data to canvas
     for (const point of points){
-      console.log(`drawing point at ${JSON.stringify(point)}`)
+      //set colour to be drawn
       ctx.fillStyle = point.colour
+
+      //render the point as a small square
       ctx.fillRect(point.point.x, point.point.y, POINT_SIZE, POINT_SIZE)
+
+      //return drawing colour to a default value
       ctx.fillStyle = "white"
     }
   }
 }
 
+/*
+ * check to see if new data is available, and if it is, the fetch interval
+ *
+ * @param force force the retrieval of data, even if it's old
+ * @return a promise to an object containing a flag for new data and the data if it exists
+ */
 async function getPoints(force = false){
-  const newDataResponse = await fetch("./newdata")
-  const newDataData = await newDataResponse.json()
-  const newData = newDataData.value
-  console.log(newData)
-  let data = []
+  // Check to see if there is new data available
+  const newDataResponse = await fetch("./newdata") //fetch /newdata endpoint
+  const newDataJson = await newDataResponse.json() //convert response to an object
+  const newData = newDataJson.value
+
+  let points = []
+
+  //If there is new data, or if the flag is true, the get the points data from the server
   if (newData || force){
-    const response = await fetch("./points")
+    const response = await fetch("./points") //fetch /points endpoint
     const jsonData = await response.json()
-    data = jsonData.points
+    points = jsonData.points
   }
 
-  return {newData, points:data}
+  return {newData, points}
 }
